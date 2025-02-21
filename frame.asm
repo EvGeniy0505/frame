b3353c0004261fd23837f82f76a6b8221a168c8e
Evgeniy Rogov <142892547+EvGeniy0505@users.noreply.github.com>
2025-02-21 13:34:37 +0300

    Update frame.asm
    text output in the frame

--- frame.asm
+++ frame.asm
@@ -5,7 +5,7 @@ model tiny
 org 100h
 
 VIDEO_SEGMENT_ADR       equ 0b800h
-FRAME_LENGTH            equ 10
+FRAME_LENGTH            equ 30
 FRAME_WIDTH             equ 8 
 
 start:                  push    VIDEO_SEGMENT_ADR
@@ -33,33 +33,74 @@ start:                  push    VIDEO_SEGMENT_ADR
                         mov     si, offset frame_template
                         mov     cx, FRAME_LENGTH
                         call    first_and_last_line
+
                         ;--------------text-----------------------
 
-                        add     di, (5 * 80 + 10) * WORD
-                        div     di, 2
+;                        add     di, (5 * 80 + 10) * WORD
+;                        mov     bl, 2
+;                        mov     ax, di
+;                        div     bl
+;                        
+;
+;
+;                        
+;                        
+;                        xor     ax, ax
+;                        xor     cx, cx
+;                        
+;                        mov     si, offset text
+;                        jmp     string_lenth
+;
+;ready_length:
+;                        mov     al, cl
+;                        mov     dl, 2
+;                        div     dl
+;                        sub     di, ax
+;                        
+;                        xor     ax, ax
+;                    
+;                        mov     ah, 52
+;                        mov     si, offset text
+;                        
+;                        add     di, (5 * 80 + 10) * WORD
+;
+;                        call    draw_text
+;
+;                        ;--------------text-----------------------
+
+                        mov     si, offset text
                         
-                        mov     ah, 52
+string_lenth:           lodsb
+                        cmp     al, 0
+                        je      ready_length
+                        inc     cl
+                        jmp     string_lenth
+
+ready_length:           mov     di, FRAME_LENGTH + FRAME_WIDTH * 80 + (5 * 80 + 10) * WORD
+                        sub     di, cx                        
+
                         mov     si, offset text
 
-                        ;добавить длину строки
-                        
-                        call draw_text
-
-                        ;--------------text-----------------------
+                        call    draw_text
 
                         mov     ah, 4ch
                         int     21h
 
-string_lenth            lodsb
-                        cmp al
 
-
+;----------------------------------------------
+; Input:    di = begining of string
+;           cx = strlen 
+; Destroys: ah, cx, si, di 
+; Returns:  cpy str 
+;----------------------------------------------
 draw_text               proc
 
                         lodsb
                         stosw
                         
-
+                        loop    draw_text   
+                        
+                        ret
                         endp
 
 first_and_last_line     proc
@@ -70,7 +111,13 @@ first_and_last_line     proc
 
                         ret
                         endp
-
+;----------------------------------------------
+; Input:    di = begining of the frame
+;           cx = frame length
+;           bx = frame width
+; Destroys: ah, cx, si, di, bx
+; Returns:  output row of frame 
+;----------------------------------------------
 draw_line               proc
 
                         mov     ah, 52                
@@ -98,10 +145,9 @@ draw_line               proc
                         ret
                         endp
 
-
 .data
 
-frame_template          db      3,32,3
-text                    db      "edik pidr$"
+frame_template          db      3,32,3   
+text                    db      "edik pidr"
 
 end start
